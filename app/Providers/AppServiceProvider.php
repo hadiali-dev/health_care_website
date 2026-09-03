0c28ce6549d714c653ee6477f85ef8eaddc9d120
hadi ali <haadii.ali.2007@gmail.com>
2026-09-03 19:31:45 +0300

    added tests units and dockerfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,17 +1,21 @@
 <?php
 
 namespace App\Providers;
- use Illuminate\Auth\Notifications\ResetPassword;
+
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-  
+    public function boot(): void
+    {
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.url') . '/reset-password/' . $token . '?email=' . urlencode($notifiable->email);
+        });
 
-public function boot(): void
-{
-    ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-        return config('app.url') . '/reset-password/' . $token . '?email=' . urlencode($notifiable->email);
-    });
-}
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
